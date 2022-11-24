b64f11f0ac0744b42e392f00434ac6ff481e0520
ᗺ0B <39238400+frychicken@users.noreply.github.com>
2022-11-24 13:05:50 -0500

    Update GUI.c

--- GUI.c
+++ GUI.c
@@ -28,13 +28,19 @@ struct MemoryStruct {
 void open_website_part(char* i) {
     char *cmd;
     #ifdef  __linux__
-    asprintf(&cmd, "xdg-open %s", i);
+    //asprintf(&cmd, "xdg-open %s", i);
+    char *tokentest = strdup(token2);
+    asprintf(&cmd, "python3 lyricart.py \"%s\"", tokentest);
     system(cmd);
     #elif __APPLE__
-    asprintf(&cmd, "OPEN %s", i);
+    //asprintf(&cmd, "OPEN %s", i);
+    char *tokentest = strdup(token2);
+    asprintf(&cmd, "python3 lyricart.py \"%s\"", tokentest);
     system(cmd);
-    #elif _WIN32__
-    asprintf(&cmd, "START %s", i);
+    #elif __WIN32__
+    //asprintf(&cmd, "START %s", i);
+    char *tokentest = strdup(token2);
+    asprintf(&cmd, "python3 lyricart.py \"%s\"", tokentest);
     system(cmd);
     #endif
 
@@ -48,14 +54,17 @@ void search_button_clicked(GtkWidget *wid,gpointer data) {
             gtk_widget_show(showLyricBtn);
 
     main2(searchData, searchData2);
-
+    
     gtk_label_set_text(GTK_LABEL(data), token2);
     gtk_entry_set_text(GTK_ENTRY(firstnameEntry),"");
     gtk_entry_set_text(GTK_ENTRY(lastnameEntry),"");
 }
 
 void Lyric_button_clicked(GtkWidget *wid,gpointer data) {
+        printf("%s", token2);
+
    open_website_part(genioslink);
+    /*
     GtkApplication* app2 = gtk_application_new ("xyz.null0verflow", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect (app2, "activate", G_CALLBACK(NULL), NULL);
 
@@ -63,10 +72,11 @@ void Lyric_button_clicked(GtkWidget *wid,gpointer data) {
     window2 = gtk_application_window_new (app2);
     gtk_window_set_title (GTK_WINDOW (window2), "Lyric");
     gtk_window_set_default_size (GTK_WINDOW (window2), 250, 600);
-    GtkWidget *box2;
+    GtkWidget *box2; 
     box2 = gtk_box_new(GTK_ORIENTATION_VERTICAL,20);
     gtk_container_add(GTK_CONTAINER(window2),box2);
     gtk_widget_show_all (window2);
+    */
 
 }
 
@@ -75,17 +85,17 @@ static void activate (GtkApplication* app, gpointer user_data) {
     window = gtk_application_window_new (app);
     gtk_window_set_title (GTK_WINDOW (window), "one of the top hits by artist");
     gtk_window_set_default_size (GTK_WINDOW (window), 500, 400);
-
+    
     GtkWidget *showSearch;
     firstnameLabel = gtk_label_new("First name:");
     firstnameEntry = gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(firstnameEntry),"first name");
-    //GIcon *icon;
+    //GIcon *icon; 
     //GFile *path;
     //path = g_file_new_for_path("");
     //icon = g_file_icon_new(path);
     //gtk_entry_set_icon_from_gicon(GTK_ENTRY(firstnameEntry),GTK_ENTRY_ICON_PRIMARY,icon);
-
+    
     lastnameLabel = gtk_label_new("Lastname:");
     lastnameEntry = gtk_entry_new();
     gtk_entry_set_placeholder_text(GTK_ENTRY(lastnameEntry),"last name");
@@ -95,12 +105,12 @@ static void activate (GtkApplication* app, gpointer user_data) {
     showLyricBtn = gtk_button_new_with_label("Show Lyric");
 
     showSearch = gtk_label_new("");
-
+    
     g_signal_connect(searchBtn,"clicked",G_CALLBACK(search_button_clicked),showSearch);
 
     g_signal_connect(showLyricBtn,"clicked",G_CALLBACK(Lyric_button_clicked),showSearch);
 
-
+    
     GtkWidget *box; box = gtk_box_new(GTK_ORIENTATION_VERTICAL,20);
     gtk_box_pack_start(GTK_BOX(box),firstnameLabel,FALSE,FALSE,0);
     gtk_box_pack_start(GTK_BOX(box),firstnameEntry,FALSE,FALSE,0);
@@ -123,19 +133,19 @@ static size_t
 WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
     size_t realsize = size * nmemb;
     struct MemoryStruct *mem = (struct MemoryStruct *)userp;
-
+    
     char *ptr = realloc(mem->memory, mem->size + realsize + 1);
     if(!ptr) {
         /* out of memory! */
         printf("not enough memory (realloc returned NULL)\n");
         return 0;
     }
-
+    
     mem->memory = ptr;
     memcpy(&(mem->memory[mem->size]), contents, realsize);
     mem->size += realsize;
     mem->memory[mem->size] = 0;
-
+    
     return realsize;
 }
 
@@ -153,7 +163,7 @@ int main(int argc,char **argv) {
     char geniusToken[] = "mXWeiTbMTOxwTP87bXlHJtTKcaam60Njvfb5OsR8XAHflIqFKvGEgGuAkXZ2dtkC";
     strcat(genius, firstname);
     strcat(genius, "%20");
-    strcat(genius, lastname);
+    strcat(genius, lastname);   
     httpsGet(geniusToken, genius);
     return 0;
 }
@@ -165,7 +175,7 @@ void httpsGet(char token[], char url[]) {
     curl = curl_easy_init();
     char authBLEH[] = "Authorization: Bearer ";
     strcpy(auth, authBLEH);
-
+    
     if (curl) {
         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
         curl_easy_setopt(curl, CURLOPT_URL, url);
@@ -178,17 +188,17 @@ void httpsGet(char token[], char url[]) {
         headers = curl_slist_append(headers, "Content-Type: application/json");
         headers = curl_slist_append(headers, "charset: utf-8");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
+        
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
-
+        
         res = curl_easy_perform(curl);
     }
-
+    
   //  printf("%s", chunk.memory);
     parseJson();
     free(chunk.memory);
-
+    
     curl_easy_cleanup(curl);
 }
 
@@ -200,11 +210,11 @@ void getLyric() {
 char *memory2 = strdup(chunk.memory);
 //strcpy(memory2, chunk.memory);
 
-    char w2[10] = "\"path";
+    char w2[10] = "\"path";  
 
 int totall = 0;
    int i2=0,k2=0,c2,index2;
- const char deli[] = "\"";
+ const char deli[] = "\""; 
 
 
 
@@ -212,7 +222,7 @@ int totall = 0;
 
            while(memory2[i2]!='\0' && clol2 == 0)
     {
-
+        
         if(memory2[i2]==w2[0])
         {
             k2=1;
@@ -224,7 +234,7 @@ int totall = 0;
                  break;
                 }
             }
-
+        
         }
         if(k2==1)
         {
@@ -232,11 +242,11 @@ int totall = 0;
                 clol2 = 1;
         }
         i2++;
-        k2=0;
+        k2=0;    
     }
         memmove(memory2, memory2+index2+8 ,strlen(memory2));
            //char *token3;
-      token3=  strtok(memory2, deli);
+      token3=  strtok(memory2, deli); 
 strcat(genioslink,token3);
       printf("%s\n", genioslink);
 
@@ -262,14 +272,14 @@ void parseJson() {
             index=i;
             clol=1;
         }
-
+        
         i++;
         k=0;
     }
-
+    
     //printf (index);
     memmove(chunk.memory, chunk.memory+index+13 ,strlen(chunk.memory));
-    const char deli[] = "\"";
-
-    token2 = strtok(chunk.memory, deli);
+    const char deli[] = "\""; 
+    
+    token2 = strtok(chunk.memory, deli); 
 }
