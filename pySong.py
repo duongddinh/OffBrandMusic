5a79fafdb2cce3b4bb0904470a41801dfb72a639
Apollo0515 <47004181+Apollo0515@users.noreply.github.com>
2022-11-29 01:48:10 -0500

    Create pySong.py

---
+++ pySong.py
@@ -0,0 +1,62 @@
+import asyncio
+import sys
+import requests
+import vlc
+
+
+
+
+def YoutubeLookup():
+    global youtubeID
+    name = str(sys.argv[1])
+    url = "https://youtube-v2.p.rapidapi.com/search/"
+
+    querystring = {"query": name, "lang": "en", "country": "us"}
+
+    headers = {
+        "X-RapidAPI-Key": "f0e0bb49b8msh380d03d27aa5fc7p19f33ajsna47ae205484d",
+        "X-RapidAPI-Host": "youtube-v2.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    json = str(response.text)
+    splitting = json.split('"')
+    youtubeID = splitting[87]
+    print(response.text)
+
+
+def YoutubeConvert():
+    global Mp3URL
+    array = []
+    youtubeURL = "https://www.youtube.com/watch?v=" + youtubeID
+    url = "https://t-one-youtube-converter.p.rapidapi.com/api/v1/createProcess"
+    querystring = {"url": youtubeURL, "format": "mp3", "responseFormat": "json", "stop": "30", "lang": "en"}
+    headers = {
+        "X-RapidAPI-Key": "f0e0bb49b8msh380d03d27aa5fc7p19f33ajsna47ae205484d",
+        "X-RapidAPI-Host": "t-one-youtube-converter.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    json = str(response.text)
+    split = json.split('"')
+    theURL = split[11]
+    ugh = theURL.split('\\')
+    s = ""
+    for i in ugh:
+        array.append(i)
+        Mp3URL = s.join(array)
+    print(response.text)
+
+async def play():
+    p = vlc.MediaPlayer(Mp3URL)
+    p.play()
+
+def main():
+    YoutubeLookup()
+    YoutubeConvert()
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(play())
+    loop.run_forever()
+    loop.close()
+
+main()
