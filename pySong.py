962051d81c6f0ef44389514681bb2e595854c14c
ᗺ0B <39238400+frychicken@users.noreply.github.com>
2022-12-05 15:59:04 -0500

    revert

--- pySong.py
+++ pySong.py
@@ -1,9 +1,8 @@
 import asyncio
 import sys
-import time
-
 import requests
 import vlc
+import time
 
 
 
@@ -25,7 +24,8 @@ def YoutubeLookup():
     json = str(response.text)
     splitting = json.split('"')
     youtubeID = splitting[87]
-    print(response.text)
+    #print(response.text)
+
 
 def YoutubeConvert():
     global Mp3URL
@@ -37,52 +37,35 @@ def YoutubeConvert():
         "X-RapidAPI-Key": "f0e0bb49b8msh380d03d27aa5fc7p19f33ajsna47ae205484d",
         "X-RapidAPI-Host": "t-one-youtube-converter.p.rapidapi.com"
     }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    json = str(response.text)
-    split = json.split('"')
-    try:
-        theURL = split[11]
-        ugh = theURL.split('\\')
-        s = ""
-        for i in ugh:
-            array.append(i)
-            Mp3URL = s.join(array)
+    while(True):
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        json = str(response.text)
+        split = json.split('"')
         print(response.text)
-    except:
-        guid = split[3];
-        YoutubeStatus(guid)
-def YoutubeStatus(guid):
-    array = []
-    youtubeURL = "https://www.youtube.com/watch?v=" + youtubeID
-    url = "https://t-one-youtube-converter.p.rapidapi.com/api/v1/statusProcess"
-
-    querystring = {"guid": guid, "responseFormat": "json", "lang": "it"}
-
-    headers = {
-        "X-RapidAPI-Key": "f0e0bb49b8msh380d03d27aa5fc7p19f33ajsna47ae205484d",
-        "X-RapidAPI-Host": "t-one-youtube-converter.p.rapidapi.com"
-    }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    json = str(response.text)
-    split = json.split('"')
-    theURL = split[11]
-    ugh = theURL.split('\\')
-    s = ""
-    for i in ugh:
-        array.append(i)
-        Mp3URL = s.join(array)
-    print(response.text)
-
-
+        try:
+            theURL = split[11]
+            ugh = theURL.split('\\')
+            s = ""
+            for i in ugh:
+                array.append(i)
+                Mp3URL = s.join(array)
+            print(Mp3URL)
+            #print(response.text)
+            break
+        except:
+            time.sleep(2)
 
 def play():
-    print(Mp3URL)
     p = vlc.MediaPlayer(Mp3URL)
     p.play()
+    time.sleep(30)
+
+def doSomething():
+    print('Hi')
 
 def main():
     YoutubeLookup()
     YoutubeConvert()
     play()
+
 main()
